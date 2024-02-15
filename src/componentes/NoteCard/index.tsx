70c00b366b1e753ab500738f37d871d7890b642f
lucasptcastro <lucasptcastro@gmail.com>
2024-02-14 22:32:38 -0300

    ✨ feat: created NoteCard component

--- src/componentes/NoteCard/index.tsx
+++ src/componentes/NoteCard/index.tsx
@@ -1,18 +1,60 @@
-export function NoteCard() {
-  return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">Há 4 dias</span>
-      <p className="text-sm leading-6 text-slate-400 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem perferendis
-        molestiae incidunt a fuga perspiciatis accusantium. Aspernatur quo
-        blanditiis facilis quam, eveniet autem fugit, totam quos deserunt modi,
-        dolorum dolor? Aspernatur quo blanditiis facilis quam, eveniet autem
-        fugit, totam quos deserunt modi, dolorum dolor? Aspernatur quo
-        blanditiis facilis quam, eveniet autem fugit, totam quos deserunt modi,
-        dolorum dolor?
-      </p>
+import * as Dialog from "@radix-ui/react-dialog";
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+// Biblioteca para formatar datas de acordo com a distância dela com a data atual (Ex.: há duas semanas, há 1 minuto)
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
+
+// Uma forma de organização colocar as variáveis referentes a nota dentro de um objeto chamado nota
+interface INoteCard {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: INoteCard) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="flex flex-col rounded-md text-left bg-slate-800 p-5 gap-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400 ">{note.content}</p>
+
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+            <p className="text-sm leading-6 text-slate-400 ">{note.content}</p>
+          </div>
+
+          {/* Aqui foi utilizada uma estilização chamada "group" que basicamente faz uma referência ao componente que ele está inserido. Então, para que eu faça uma animação de underline no texto do span (filho) quando passar o mouse sobre o botão (pai), precisa colocar o group no elemento pai e o group + estilização no elemento filho que eu desejo criar a ação de underline, no caso o span */}
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota
+            </span>{" "}
+            ?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
