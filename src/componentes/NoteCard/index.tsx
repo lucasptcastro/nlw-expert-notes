5eda6724d35d48ae87e6efa7c89e1550073a35ab
lucasptcastro <lucasptcastro@gmail.com>
2024-02-15 21:45:18 -0300

    ✨ feat: ajusted responsiveness and date format

--- src/componentes/NoteCard/index.tsx
+++ src/componentes/NoteCard/index.tsx
@@ -8,26 +8,31 @@ import { X } from "lucide-react";
 // Uma forma de organização colocar as variáveis referentes a nota dentro de um objeto chamado nota
 interface INoteCard {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+  onNoteDeleted: (id: string) => void;
 }
 
-export function NoteCard({ note }: INoteCard) {
+export function NoteCard({ note, onNoteDeleted }: INoteCard) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="flex flex-col rounded-md text-left bg-slate-800 p-5 gap-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400 ">{note.content}</p>
 
-        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+        <Dialog.Content className="fixed inset-0 flex w-full flex-col overflow-hidden bg-slate-700 outline-none md:inset-auto md:left-1/2 md:top-1/2 md:h-[60vh] md:max-w-[640px] md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-md">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
@@ -45,7 +50,8 @@ export function NoteCard({ note }: INoteCard) {
           {/* Aqui foi utilizada uma estilização chamada "group" que basicamente faz uma referência ao componente que ele está inserido. Então, para que eu faça uma animação de underline no texto do span (filho) quando passar o mouse sobre o botão (pai), precisa colocar o group no elemento pai e o group + estilização no elemento filho que eu desejo criar a ação de underline, no caso o span */}
           <button
             type="button"
-            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+            onClick={() => onNoteDeleted(note.id)}
+            className="group w-full bg-slate-800 py-4 text-center text-sm font-medium text-slate-300 outline-none"
           >
             Deseja{" "}
             <span className="text-red-400 group-hover:underline">
